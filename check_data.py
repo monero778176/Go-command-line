58dc03628dd86d896c729f3639d052f7606c92db
monero778176 <41115595+monero778176@users.noreply.github.com>
2024-10-26 16:45:05 +0800

    新增加入資料庫以及查詢功能

--- check_data.py
+++ check_data.py
@@ -5,7 +5,7 @@ from nicegui import ui
 from nicegui import Tailwind, ui
 import pyperclip
 import pandas as pd
-
+from database_go import *
 
 ui.add_head_html('<link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">')
 
@@ -53,60 +53,126 @@ class commend_list(rxui.ViewModel):
         
         return final_c.value
 
+# 顯示資料庫用的
+d_opter = database_opertor()  # database create, init
+cs_result = None
 
+# show db search result, and ui refresh
+@ui.refreshable
+def show_search_result_ui(table_name,cat=''):
+    cs_result =  d_opter.search(table_name,cat)
+    print(f'跑進更新環節,cs:{cs_result}')
+    if cs_result!=None:
+        with ui.column():
+            for rr in cs_result:
+                with ui.row():
+                    ui.label(rr.id).classes('mt-2')
+                    ui.code(rr.value).classes('w-60')
+                    ui.label(rr.category).classes('mt-2')
+                    ui.label(rr.descript).classes('mt-2')
+
+
+
+# custom category save to database
+def save_cate_to_database_ui():
+    with ui.card().bind_visibility_from(save_,'value').classes('w-200'):
+        ui.label(f'會被存入的value').tailwind.font_weight('extrabold').text_color('orange-400').\
+                font_size('2xl').text_underline_offset('2px')
+        rxui.label(lambda: f' {cList.display_commend.value}')
+
+        save_cat_input = ui.input(label='category')  # 輸入要儲存的類別
+
+        rxui.button('save',on_click=lambda:save_commend_to_database(cList.display_commend.value, save_cat_input.value) )
+        save_cat_input.set_value('')
+
+def copy_and_notify(label_):
+    pyperclip.copy(label_)
+    ui.notify('Copy the commend')
+    
+
+
+#commend list
 cList = commend_list()
 cList.add('python')
 
-with ui.card().classes('w-200'):
+with ui.row():
+    with ui.card().classes('w-200'):
 
-    rxui.label('commend generator')
-    
-    @rxui.vfor(cList.comList)  # for 迴圈 list
-    def _(store:rxui.VforStore[pair_param]):
-        p_item = store.get_item()
+        rxui.label('commend generator')
         
+        @rxui.vfor(cList.comList)  # for 迴圈 list
+        def _(store:rxui.VforStore[pair_param]):
+            p_item = store.get_item()
+            
 
-        with ui.row().classes('justify-center'):
-            rxui.checkbox(value=p_item.selected).classes('w-10').classes('justify-center m-auto')
-            rxui.input(value=p_item.name,placeholder='參數名',on_change=lambda:cList.display_commend).classes('w-20')
-            rxui.input(value=p_item.value,placeholder='值',on_change=lambda:cList.display_commend).classes('w-80')
-
-
-    # add_item = rxui.input(value='', placeholder='參數名')
-    with ui.row():
-        ui.label('新增參數(enter)').classes('justify-center m-auto')
-        add_item = ui.input(value='', placeholder='參數名')
-
-        # 事件處理
-    add_item.on('keydown.enter',lambda : (cList.add(add_item.value,'')))
-    add_item.on('keydown.enter',lambda : add_item.set_value(' '))
-    # add_item.on('keydown.enter',lambda : )
+            with ui.row().classes('justify-center'):
+                rxui.checkbox(value=p_item.selected).classes('w-10').classes('justify-center m-auto')
+                rxui.input(value=p_item.name,placeholder='參數名',on_change=lambda:cList.display_commend).classes('w-20')
+                rxui.input(value=p_item.value,placeholder='值',on_change=lambda:cList.display_commend).classes('w-80')
 
 
-    rxui.label('完整的commend line')
-    with ui.row():
+        # add_item = rxui.input(value='', placeholder='參數名')
+        with ui.row():
+            # ui.label().classes('justify-center m-auto')
+            add_item = ui.input(label='新增參數(enter)', placeholder='參數名')
+
+            # 事件處理
+        add_item.on('keydown.enter',lambda : (cList.add(add_item.value,'')))
+        add_item.on('keydown.enter',lambda : add_item.set_value(' '))
+        # add_item.on('keydown.enter',lambda : )
+
+
+        rxui.label('完整的commend line')
         label_now = rxui.label(lambda: f"{cList.display_commend.value}").style('background: #C0C0C0; padding:5px')
+        with ui.row():
+            rxui.button('copy',on_click=lambda: copy_and_notify(label_now.text)) 
+            # rxui.button('save',)
+            save_=rxui.checkbox('save detail',value=False)
+            search_=rxui.checkbox('Search',value=False)
 
-        rxui.button('copy',on_click=lambda: copy_and_notify(label_now.text)) 
-        rxui.button('save',)
+        # 這裡要從資料庫挖相對應的類別set 以及現有的資料表
+        
+        
     
+    # 用來做儲存到資料庫的
+    # with ui.card().bind_visibility_from(save_,'value').classes('w-200'):
+    #     ui.label(f'會被存入的value').tailwind.font_weight('extrabold').text_color('orange-400').\
+    #             font_size('2xl').text_underline_offset('2px')
+    #     rxui.label(lambda: f' {cList.display_commend.value}')
+    #     save_cat_input = ui.input(label='category')
+
+    #     rxui.button('save',on_click=lambda:save_commend_to_database(cList.display_commend.value,'python') )
+
+    with ui.grid():
+        save_cate_to_database_ui()
+
+    # 用作搜尋資料庫的
+    with ui.card().bind_visibility_from(search_,'value'):
+        list_table_name =  [key for key in table_dict.keys()]
+        cate_set = ['python']
+        with ui.row():
+            # ui.label('選擇資料表')
+            select_table = ui.select(list_table_name, multiple=True, value=list_table_name[0], label='選擇資料表') \
+                .classes('w-64').props('use-chips')
+
+            ## 許則資料表下的屬性
+            select_cat = ui.select(cate_set, multiple=False, value=cate_set[0], label='選擇類型') \
+                .classes('w-64').props('use-chips')
 
 
-## 按鈕事件: 觸發複製當前結果的功能
-def copy_and_notify(x):
-    pyperclip.copy(x)
-    ui.notify("Has copy commend")
+        print(f'選擇的value:{select_table.value[0]}')
+        print(f'選擇類別的value:{select_cat.value}')
+        rxui.button('search',on_click = lambda: show_search_result_ui.refresh(select_table.value[0],select_cat.value))
+        show_search_result_ui(cs_result)   # refresh search ui section
+# with ui.card():
+
+# 用以儲存 cat 指令
+def save_commend_to_database(value,category):
+    c_item = Commend(value=value,category=category,descript='')
+    d_opter.insert_sepcify(c_item)
+    ui.notify('Sussece save to database')
+    # save_cat_input.set_value('')
 
 
-
-
-data = pd.DataFrame({"name": ["f", "a", "c", "b"], "age": [1, 2, 3, 1]})
-ds = bi.data_source(data)
-
-ds.ui_table(
-    columns=[
-        {"label": "new colA", "field": "colA", "sortable": True},
-    ]
-)
 
 ui.run()
--- database_go.py
+++ database_go.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, create_engine, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import insert
+from sqlalchemy import insert,delete
 
 
 # 创建对象的基类:
@@ -25,8 +25,9 @@ class Commend(Base):
 
     # 表的结构:
     id = Column(Integer, primary_key=True,autoincrement=True)
-    source_name = Column(String(20))
     value = Column(String(100))
+    category = Column(String(20))
+    descript = Column(String(50))
 
 # 初始化数据库连接:
 # engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/test')
@@ -45,7 +46,7 @@ class Commend(Base):
 # print('name:', user.name)
 # # 关闭Session:
 # session.close()
-
+table_dict = {'commend':Commend,'user':User}
 
 class database_opertor:
     def __init__(self) -> None:
@@ -54,12 +55,31 @@ class database_opertor:
         self.engine = create_engine('sqlite:///test.db')
         Base.metadata.create_all(self.engine)
         self.DBSession = sessionmaker(bind=self.engine)
-        
+        self.session = self.DBSession()
 
     # 創建相關的資料庫
     # def create_table(self):
 
     #     Base.metadata.create_all(self.engine)  # 包含裡面所存在資料表
+
+    def search(self,table_name,cat_value:str=''):
+        # cs = self.session.query(table_dict[table_name]).filter_by(category=cat_value).all()
+
+        try:
+            print(f'試著查找資料表:{table_name}')
+            print(f'進入db search::{cat_value}')
+            if cat_value!='':
+                print(f'查詢資料表:{table_name} 搜尋類別:{cat_value}')
+                cs = self.session.query(table_dict[table_name]).filter_by(category=cat_value).all()
+                print(f'回傳cs:{cs}')
+            else:
+                print(f'不具有類別')
+                cs = self.session.query(table_dict[table_name]).all()
+
+        except:
+            cs = None
+            print(f'搜尋上出點事情')
+        return cs
 
     def insert(self,item):
         session = self.DBSession()
@@ -67,13 +87,36 @@ class database_opertor:
         session.commit()
         session.close()
 
+    def insert_sepcify(self,c_item):
+        if c_item is not None:
+            session = self.DBSession()
+            # id,value,cat,des = c_item
+
+            session.add(c_item)
+            session.commit()
+            session.close()
+            print(f'成功儲存資料到資料庫')
+
+
+    def delete(self,table_name:str,id:int):
+        
+        session = self.DBSession()
+        
+        delete_item = delete(table_dict[table_name]).where(table_dict[table_name].id==id)
+        print(f'要刪除的項目:{delete_item}')
+        session.delete(delete_item)
+        session.commit()
+        session.close()
+
+    def drop_table(self):
+        Base.metadata.drop_all(bind=self.engine, tables=[Commend.__table__])
 
 
 if __name__=='__main__':
 
     d_operator = database_opertor()
-
-    c_item = Commend(source_name='test',value='ttest')
+    # d_operator.drop_table()
+    c_item = Commend(value='test',category='ttest',descript='None temp')
     d_operator.insert(c_item)
 
 
